afc22041a77c8c2cf24d0e66cd3e7a56e3aa93d8
wils0040 <nicholas.n.wilson-1@ou.edu>
2020-02-03 11:07:35 -0600

    Almost completed WPF assignment.

--- WPF1/WPF1/Class1.cs
+++ WPF1/WPF1/Class1.cs
@@ -8,11 +8,38 @@ namespace WPF1
 {
     class EntryForm
     {
-        public string name { get; set; }
+        public string Name { get; set; }
         
-        public string address { get; set; }
+        public string Address { get; set; }
 
-        public int zip { get; set; }
+        public int Zip { get; set; }
+
+        public EntryForm()
+        {
+            Name = String.Empty;
+            Address = String.Empty;
+            Zip = 0;
+        }
+        
+        public EntryForm(string name, string address, int zip)
+        {
+            Name = name;
+            Address = address;
+            zip = Zip;
+        }
+        
+        public EntryForm(string address, int zip)
+        {
+            Name = string.Empty;
+            Address = address;
+            Zip = zip;
+        }
+
+
+        public override string ToString()
+        {
+            return $"Name: {Name} Address: {Address} Zip: {Zip}";
+        }
 
 
     }
--- WPF1/WPF1/MainWindow.xaml.cs
+++ WPF1/WPF1/MainWindow.xaml.cs
@@ -24,35 +24,46 @@ namespace WPF1
         public MainWindow()
         {
             InitializeComponent();
+            tb1.Text = string.Empty;
+            tb2.Text = string.Empty;
+            tb3.Text = string.Empty;
         }
         
+        private void Submit_Click(object sender, RoutedEventArgs e)
+        {
+            string Namy = tb1.Text;
+            string Addy = tb2.Text;
+            string Zipy = tb3.Text;  // equivalent to doing Console.ReadLine();
+            int ZipCode;
+
+            
+            if (string.IsNullOrWhiteSpace(Namy) == true)
+            {
+                MessageBox.Show("You must enter your full name");
+            }
+
+            bool isInt = Int32.TryParse(Zipy, out ZipCode);
+            if (isInt == false)
+            {
+                MessageBox.Show("Must enter integer value for ZipCode");
+            }
+
+            /* Create an instance of the EntryForm object using the empty construtor
+                EntryForm ef = new EntryForm()
+                ef.Address = addy;
+                ef.Name = namy;
+                ef.ZipCode = Zip;
+            */
+
+            // Create an instance of the EntryForm object 
+            EntryForm ef = new EntryForm(Namy, Addy, ZipCode);
 
 
 
+        }
 
         private void Tb1_TextChanged(object sender, TextChangedEventArgs e)
         {
-            
-            
-        }
-        
-        private void Tb2_TextChanged(object sender, TextChangedEventArgs e)
-        {
-            
-        }
-
-        private void Tb3_TextChanged(object sender, TextChangedEventArgs e)
-        {
-            
-        }
-
-        private void Submit_Click(object sender, RoutedEventArgs e)
-        {
-            string name = tb1.Text;
-            string address = tb2.Text;
-            int zip = Convert.ToInt32(tb3.Text);
-
-            
 
         }
     }
